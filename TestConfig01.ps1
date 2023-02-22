57f9571254448a0b95e5b3a3d616b40adcc9fa37
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-02-22 12:59:26 -0600

    Update TestConfig01.ps1

--- TestConfig01.ps1
+++ TestConfig01.ps1
@@ -1,3 +1,1 @@
 ﻿Install-WindowsFeature -Name Web-Server -IncludeManagementTools 
-Remove-Item C:\\inetpub\\wwwroot\\iisstart.htm
-Add-Content -Path "C:\\inetpub\\wwwroot\\iisstart.htm" -Value $("Hello from " + $env:computername)
