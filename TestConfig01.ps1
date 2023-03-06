0a39fe22c8008e48e6a51291a6bc86fb164e9057
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-06 14:19:27 -0600

    Update TestConfig01.ps1

--- TestConfig01.ps1
+++ TestConfig01.ps1
@@ -1,3 +1,4 @@
 Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
 Format-Volume -DriveLetter F
+Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'}
