0d31d6f8b5f9cfe118e23d1c13621b71865f0681
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-06 13:46:39 -0600

    Update WebRoleInstall.ps1

--- WebRoleInstall.ps1
+++ WebRoleInstall.ps1
@@ -1,3 +1,4 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter E -UseMaximumsize
+Format-Volume -DriveLetter F -FileSystem NTFS
