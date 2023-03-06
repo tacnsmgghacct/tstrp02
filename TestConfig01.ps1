0b9dec764cfcd51ea945ea6f79b4ec4039f0ec6d
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-06 13:46:04 -0600

    Update TestConfig01.ps1

--- TestConfig01.ps1
+++ TestConfig01.ps1
@@ -1,2 +1,3 @@
 Initialize-Disk -Number 2 -PartitionStyle GPT
-New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize 
+New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
+Format-Volume -DriveLetter F -Filesystem NTFS
