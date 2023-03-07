f5d02625acf89d71604cc34bdd6623be2554c35e
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-07 14:28:25 -0600

    Update WebRoleInstall.ps1

--- WebRoleInstall.ps1
+++ WebRoleInstall.ps1
@@ -1,4 +1,13 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Install-WindowsFeature -Name Web-Basic-Auth
+Install-WindowsFeature -Name Web-Digest-Auth
+Install-WindowsFeature -Name Web-Windows-Auth
+Install-WindowsFeature -Name Web-App-Dev
+Install-WindowsFeature -Name Web-Net-Ext
+Install-WindowsFeature -Name Web-Net-ext45
+Install-WindowsFeature -Name Web-Appinit
+Install-WindowsFeature -Name Web-ASP, Web-ASP-Net, Web-ASP-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter
+Install-WindowsFeature -Name Web-Mgmt-Console, Web-Metabase
 Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
 Format-Volume -DriveLetter F
