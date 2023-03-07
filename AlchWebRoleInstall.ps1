e02fc310ba88ce9d4fba9f458b5cf2a3fc24549a
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-07 15:19:53 -0600

    Update and rename WebRoleInstall.ps1 to AlchWebRoleInstall.ps1

--- AlchWebRoleInstall.ps1
+++ AlchWebRoleInstall.ps1
@@ -8,6 +8,8 @@ Install-WindowsFeature -Name Web-Net-ext45
 Install-WindowsFeature -Name Web-Appinit
 Install-WindowsFeature -Name Web-ASP, Web-ASP-Net, Web-ASP-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter
 Install-WindowsFeature -Name Web-Mgmt-Console, Web-Metabase
+Install-WindowsFeature -Name NET-HTTP-Activation, NET-Non-HTTP-Activ
+Install-WindowsFeature -Name NET-Framework-45-Features, NET-Framework-45-Core, NET-Framework-45-ASPNET, NET-WCF-Services45, NET-WCF-TCP-PortSharing45
 Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
 Format-Volume -DriveLetter F
