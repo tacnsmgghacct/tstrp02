030415ef15f1b55e4aa63e19543cbd1010a2e3e4
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-17 10:30:37 -0500

    Delete TestConfig01.ps1

--- TestConfig01.ps1
+++
@@ -1,5 +0,0 @@
-Set-Timezone -Name 'Central Standard Time'
-Initialize-Disk -Number 2 -PartitionStyle GPT
-New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
-Format-Volume -DriveLetter F
-Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'}
