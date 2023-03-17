4fef7bd1055aadd83da2649b935447fbecc90c22
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-17 10:30:22 -0500

    Create BaseConfig.ps1

---
+++ BaseConfig.ps1
@@ -0,0 +1,5 @@
+Set-Timezone -Name 'Central Standard Time'
+Initialize-Disk -Number 2 -PartitionStyle GPT
+New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
+Format-Volume -DriveLetter F
+Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'} 
