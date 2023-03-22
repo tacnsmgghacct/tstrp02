b3c39a4b07eab6dd078792b4e59f469ac1c5b98b
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-03-22 10:43:33 -0500

    Update BaseConfig.ps1

--- BaseConfig.ps1
+++ BaseConfig.ps1
@@ -3,3 +3,4 @@ Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
 Format-Volume -DriveLetter F
 Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'} 
+Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6'
