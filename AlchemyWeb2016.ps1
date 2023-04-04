beb8bc8d6fd11cc9c4b69e36162ee53a3fa54861
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-04-04 10:13:49 -0500

    Create AlchemyWeb2016.ps1

---
+++ AlchemyWeb2016.ps1
@@ -0,0 +1,14 @@
+Set-Timezone -Name 'Central Standard Time'
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+Install-WindowsFeature -Name Web-Basic-Auth
+Install-WindowsFeature -Name Web-Digest-Auth
+Install-WindowsFeature -Name Web-Windows-Auth
+Install-WindowsFeature -Name Web-App-Dev
+Install-WindowsFeature -Name Web-Net-Ext
+Install-WindowsFeature -Name Web-Net-ext45
+Install-WindowsFeature -Name Web-Appinit
+Install-WindowsFeature -Name Web-ASP, Web-ASP-Net, Web-ASP-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter
+Install-WindowsFeature -Name Web-Mgmt-Console, Web-Metabase
+Install-WindowsFeature -Name NET-HTTP-Activation, NET-Non-HTTP-Activ
+Install-WindowsFeature -Name NET-Framework-45-Features, NET-Framework-45-Core, NET-Framework-45-ASPNET, NET-WCF-Services45, NET-WCF-TCP-PortSharing45
+Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6'
