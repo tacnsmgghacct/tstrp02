4d89f006ea5fd42fda5f4cbe283d40661641740d
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-04-17 11:23:02 -0500

    Update AlchWebRoleInstall.ps1

--- AlchWebRoleInstall.ps1
+++ AlchWebRoleInstall.ps1
@@ -11,8 +11,4 @@ Install-WindowsFeature -Name Web-ASP, Web-ASP-Net, Web-ASP-Net45, Web-ISAPI-Ext,
 Install-WindowsFeature -Name Web-Mgmt-Console, Web-Metabase
 Install-WindowsFeature -Name NET-HTTP-Activation, NET-Non-HTTP-Activ
 Install-WindowsFeature -Name NET-Framework-45-Features, NET-Framework-45-Core, NET-Framework-45-ASPNET, NET-WCF-Services45, NET-WCF-TCP-PortSharing45
-Initialize-Disk -Number 2 -PartitionStyle GPT
-New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
-Format-Volume -DriveLetter F
-Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'} 
 Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6'
