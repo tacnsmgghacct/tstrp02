501ef2e009e3c56939f90ab2e135bacdf415dc69
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-07-19 16:13:21 -0500

    Update DCRoleInstall.ps1

--- DCRoleInstall.ps1
+++ DCRoleInstall.ps1
@@ -3,5 +3,5 @@ Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
 Format-Volume -DriveLetter E
 Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'E:'" | Set-CimInstance -Property @{Label='Data'} 
-Disable-NetAdapterBinding -Name 'Ethernet 2' -ComponentID 'ms_tcpip6'
+Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6'
 Add-WindowsFeature AD-Domain-Services
