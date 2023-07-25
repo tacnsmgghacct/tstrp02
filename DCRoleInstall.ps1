acd485b8a950eb67c2191aaf09df3b88db70cb2c
tacnsmgghacct <122573676+tacnsmgghacct@users.noreply.github.com>
2023-07-25 16:14:25 -0500

    Update DCRoleInstall.ps1

--- DCRoleInstall.ps1
+++ DCRoleInstall.ps1
@@ -1,7 +1,7 @@
 Set-Timezone -Name 'Central Standard Time'
 Initialize-Disk -Number 2 -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumsize
-Format-Volume -DriveLetter E
-Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'E:'" | Set-CimInstance -Property @{Label='Data'} 
+Format-Volume -DriveLetter F
+Get-CIMInstance -ClassName Win32_Volume -Filter "DriveLetter = 'F:'" | Set-CimInstance -Property @{Label='Data'} 
 Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6'
 Add-WindowsFeature AD-Domain-Services
